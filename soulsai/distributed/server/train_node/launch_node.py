9f5de1d0f753a6830e5b465fa387818e432e2911
Martin Schuck <martin.schuck@tum.de>
2022-07-09 16:29:05 +0200

    Fix missing 'save' directory

--- soulsai/distributed/server/train_node/launch_node.py
+++ soulsai/distributed/server/train_node/launch_node.py
@@ -8,6 +8,7 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     path = Path(__file__).parent / "save" / "training_node.log"
+    path.parent.mkdir(exist_ok=True)
     logging.basicConfig(level=logging.INFO)
     logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s")
     fileHandler = logging.FileHandler(path)
