6902b40aebae3acca24c3cbcb213b9c6ea623c02
Martin Schuck <martin.schuck@tum.de>
2023-02-07 17:12:11 +0000

    Add client shutdown script

---
+++ scripts/client_shutdown.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from redis import Redis
+
+from soulsai.utils import load_redis_secret
+
+
+def main():
+    config_dir = Path(__file__).parents[1] / "config"
+    secret = load_redis_secret(config_dir / "redis.secret")
+    red = Redis(host="localhost", port=6379, password=secret, db=0, decode_responses=True)
+    red.publish("client_shutdown", 1)
+
+
+if __name__ == "__main__":
+    main()
