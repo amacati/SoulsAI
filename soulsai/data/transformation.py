afa4e0e9540a020657044c1012c31b26b92bdebe
Martin Schuck <martin.schuck@tum.de>
2023-06-29 00:30:35 +0200

    Simplify GameStateTransformer

--- soulsai/data/transformation.py
+++ soulsai/data/transformation.py
@@ -31,20 +31,19 @@ class GameStateTransformer:
         self.game_id = game_id
         self.boss_id = boss_id
         self.game_data = StaticGameData(game_id)
-        # Get all movement animations with their ID
-        all_boss_animations = self.game_data.boss_animations[self.boss_id]["all"]
-        boss_movements = [a["ID"] for a in all_boss_animations.values() if a["type"] == "movement"]
-        self.boss_move_animations = boss_movements
+        # Initialize player one-hot encoder
         self.player_animation_encoder = OneHotEncoder(allow_unknown=True)
         p_animations = [a["ID"] for a in self.game_data.player_animations.values()]
         filtered_player_animations = unique(map(self.filter_player_animation, p_animations))
         self.player_animation_encoder.fit(filtered_player_animations)
+        # Initialize boss one-hot encoder
         self.boss_animation_encoder = OneHotEncoder(allow_unknown=True)
-        boss_animations = [a["ID"] for a in all_boss_animations.values()]
+        iudex_animations = self.game_data.boss_animations[self.boss_id]["all"]
+        boss_animations = [a["ID"] for a in iudex_animations.values()]
         filtered_boss_animations = unique(
             map(lambda x: self.filter_boss_animation(x)[0], boss_animations))
         self.boss_animation_encoder.fit(filtered_boss_animations)
-
+        # Initialize stateful attributes
         self._current_time = 0.
         self._acuumulated_time = 0.
         self._last_animation = None
@@ -68,7 +67,7 @@ class GameStateTransformer:
         # 4-15: player_pos, player_rot, boss_pos, boss_rot, camera_rot
         # 16-17: player_animation_duration, boss_animation_duration
         # 17-48: player_animation_onehot
-        # 49-70: boss_animation_onehot
+        # 49-73: boss_animation_onehot
         obs = self._unpack_obs(obs)
         player_animation = self.filter_player_animation(obs["player_animation"])
         player_animation_onehot = self.player_animation_encoder(player_animation)
@@ -206,7 +205,9 @@ class GameStateTransformer:
         Returns:
             The animation name and a flag set to True if it was binned (else False).
         """
-        if animation in self.boss_move_animations:
+        # <WalkFront, WalkLeft, WalkRight, WalkBack, TurnRight90, TurnRight180, TurnLeft90,
+        # TurnLeft180>
+        if animation in [19, 20, 21, 22, 24, 25, 26, 27]:
             return 100, True
         return animation, False
 
