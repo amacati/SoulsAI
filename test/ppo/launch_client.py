a18ee72aa5851967771076d94a5f9d9d93403ba4
Martin Schuck <martin.schuck@tum.de>
2023-11-08 08:29:12 +0100

    Fix launch sequences and configs for atari and ppo

--- test/ppo/launch_client.py
+++ test/ppo/launch_client.py
@@ -2,9 +2,11 @@
 from pathlib import Path
 
 from soulsai.distributed.client.ppo_client import ppo_client
-from soulsai.utils import load_config
+from soulsai.utils import load_config, load_remote_config, load_redis_secret
 
 if __name__ == "__main__":
     root_dir = Path(__file__).parents[2] / "config"
-    config = load_config(root_dir / "config_d.yaml", root_dir / "config.yaml")
+    local_config = load_config(root_dir / "config_d.yaml", root_dir / "config.yaml")
+    secret = load_redis_secret(Path("/run/secrets/redis_secret"))
+    config = load_remote_config(local_config.redis_address, secret, local_config=local_config.local)
     ppo_client(config)
