50ac5ad545d2b3db4c2e4694590da7a77906e766
Martin Schuck <martin.schuck@tum.de>
2023-12-28 10:47:10 +0100

    Fix quicksave and client shutdown scripts

--- scripts/quicksave.py
+++ scripts/quicksave.py
@@ -12,6 +12,6 @@ from soulsai.utils import load_redis_secret
 
 if __name__ == "__main__":
     config_dir = Path(__file__).parents[1] / "config"
-    secret = load_redis_secret(config_dir / "redis.secret")
+    secret = load_redis_secret(config_dir / "secrets/redis.secret")
     red = Redis(host="localhost", port=6379, password=secret, db=0, decode_responses=True)
     red.publish("manual_save", 1)
