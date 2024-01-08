86c279a4f40ac367584e24d6c67f990e011ffdfb
Martin Schuck <martin.schuck@tum.de>
2024-01-08 13:23:05 +0100

    Add GrayScaleObservation wrapper

--- soulsai/wrappers/__init__.py
+++ soulsai/wrappers/__init__.py
@@ -1,6 +1,7 @@
 from gymnasium.wrappers import ResizeObservation, FrameStack, AtariPreprocessing
+from gymnasium.wrappers.gray_scale_observation import GrayScaleObservation
 
-from soulsai.wrappers.common import ReorderChannels, MaterializeFrames
+from soulsai.wrappers.common import ReorderChannels, MaterializeFrames, CenterCropFrames
 from soulsai.wrappers.atari import AtariExpandImage
 # Docker containers for testing don't install soulsgym. Wrappers that depend on it are only imported
 # if soulsgym is installed.
@@ -12,7 +13,7 @@ except ModuleNotFoundError:
 
 __all__ = [
     "ReorderChannels", "MaterializeFrames", "ResizeObservation", "FrameStack", "AtariPreprocessing",
-    "AtariExpandImage"
+    "AtariExpandImage", "CenterCropFrames", "GrayScaleObservation"
 ]
 if soulsgym_installed:
     __all__.append("IudexObservationWrapper")
