71c06e76ba6ee76828771d4b97770dbb6ae537b5
Martin Schuck <martin.schuck@tum.de>
2024-01-14 16:40:40 +0100

    Fix observation spaces of wrappers

--- soulsai/wrappers/common.py
+++ soulsai/wrappers/common.py
@@ -15,8 +15,17 @@ class ReorderChannels(ObservationWrapper):
 
     def __init__(self, env: Env, reorder_pattern: str = "s w h c -> (s c) w h"):
         super().__init__(env)
-        # TODO: Recalculate observation shape
+        assert isinstance(self.observation_space, Box)
         self.reorder_pattern = reorder_pattern
+        # Calculate new observation space
+        low, high = self.observation_space.low, self.observation_space.high
+        if isinstance(low, np.ndarray):
+            low = einops.rearrange(low, reorder_pattern)
+        if isinstance(high, np.ndarray):
+            high = einops.rearrange(high, reorder_pattern)
+        shape = einops.rearrange(self.observation_space.sample(), reorder_pattern).shape
+        dtype = self.observation_space.dtype
+        self.observation_space = Box(low=low, high=high, shape=shape, dtype=dtype)
 
     def observation(self, observation: np.ndarray) -> np.ndarray:
         return einops.rearrange(observation, self.reorder_pattern)
@@ -41,16 +50,17 @@ class CenterCropFrames(ObservationWrapper):
         assert all(i >= o for i, o in zip(input_shape, output_shape))
         self.input_shape = input_shape
         left_crop = [(i - o) // 2 for i, o in zip(input_shape, output_shape)]
-        self.slices = tuple(slice(l, l + o) for l, o in zip(left_crop, output_shape))
+        self.slices = tuple(slice(i, i + j) for i, j in zip(left_crop, output_shape))
         # Calculate new observation space
-        obs_shape = self.observation_space.shape
-        new_shape = [s.indices(obs_shape[i])[1] - s.indices(obs_shape[i])[0]
-                     for i, s in enumerate(self.slices)]
         low = self.observation_space.low[self.slices]
         high = self.observation_space.high[self.slices]
+        obs_shape = self.observation_space.shape
+        shape = [
+            s.indices(obs_shape[i])[1] - s.indices(obs_shape[i])[0]
+            for i, s in enumerate(self.slices)
+        ]
         dtype = self.observation_space.dtype
-        self.observation_space = Box(low=low, high=high, shape=new_shape, dtype=dtype)
-
+        self.observation_space = Box(low=low, high=high, shape=shape, dtype=dtype)
 
     def observation(self, observation: np.ndarray) -> np.ndarray:
         assert observation.shape == self.input_shape
